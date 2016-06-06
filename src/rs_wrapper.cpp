1661df70637141c8f8ce0ff8fa46bbbb9e330401
Jimmy Li <jimmy.jiajian.li@gmail.com>
2016-06-06 15:26:36 -0400

    Working build with library included

--- src/rs_wrapper.cpp
+++ src/rs_wrapper.cpp
@@ -1,5 +1,6 @@
 #include "rs_wrapper.h"
 #include <iostream>
+#include <rs.hpp>
 
 int addTwo(int a, int b) {
     return a+b;
@@ -8,5 +9,6 @@ int addTwo(int a, int b) {
 int main(void) {
     int result = addTwo(1,2);
     std::cout << result;
+	rs::context ctx;
     return result;
 }
