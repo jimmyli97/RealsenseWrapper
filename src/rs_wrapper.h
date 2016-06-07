3b17d6b410a230a010532b1797e95298abb4e222
Jimmy Li <jimmy.jiajian.li@gmail.com>
2016-06-07 10:36:04 -0400

    Add header prototypes, working binary compilation

--- src/rs_wrapper.h
+++ src/rs_wrapper.h
@@ -1,6 +1,44 @@
 #ifndef RSWRAPPER_H
 #define RSWRAPPER_H
 
-int addTwo(int, int);
+#include <vector>
+#include <string>
+
+#include <rs.hpp>
+
+class RealSenseWrapper {
+public:
+	enum RSError {
+	};
+
+	RealSenseWrapper() {
+		// Initialize context, initialize list of cameras
+	}
+
+	~RealSenseWrapper() {
+		// Stop all streams, close all cameras, destroy context
+	}
+
+	/// Returns list of serial names each corresponding to a camera
+	std::vector<std::string> getDeviceList();
+
+	void printStatus();
+
+	/// Returns frame at specified timestamp of specified stream
+	RSError getFrame(const void * frame, std::string serial, rs::stream strm,
+					 std::string streamName, int timestamp);
+
+	RSError startStream(std::string serial, rs::stream strm, std::string streamName, rs::preset preset);
+
+	RSError startStream(std::string serial, rs::stream strm, std::string streamName,
+					 int width, int height, rs::format fmt, int framerate);
+
+	RSError stopStream(std::string serial, rs::stream strm);
+
+private:
+	rs::device * getDevice(std::string serial);
+
+	std::vector<rs::device *> _devices;
+};
 
 #endif
