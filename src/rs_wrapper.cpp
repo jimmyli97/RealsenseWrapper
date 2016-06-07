e594771078eb5104186c16effd7b320b64448e39
Jimmy Li <jimmy.jiajian.li@gmail.com>
2016-06-07 17:02:11 -0400

    Added boost lib, realsense dlls/libs, code for dir scanning

--- src/rs_wrapper.cpp
+++ src/rs_wrapper.cpp
@@ -1,14 +1,65 @@
-#include "rs_wrapper.h"
 #include <iostream>
+#include <string>
 #include <rs.hpp>
 
-int addTwo(int a, int b) {
-    return a+b;
+#include "rs_wrapper.h"
+
+namespace rsw {
+	RealSenseWrapper::RealSenseWrapper(std::string directory) :
+									   _deviceMap(), dataPath(directory) {
+		//rs::context ctx;
+		/*// Open directory, check validity
+		rs::log_to_console(rs::log_severity::warn);
+
+		if (fs::exists(dataPath) && fs::is_directory(dataPath)) {
+			// Initialize mapping
+			for (auto dirEnt : fs::directory_iterator(dataPath)) {
+				std::string fn = dirEnt.path().filename().string();
+				std::cout << "Found recordings for device: " << fn << std::endl;
+				_deviceMap[fn] = NULL;
+			}
+
+			// Find matching connected devices, add to map
+			for (int i = 0; i < ctx.get_device_count(); ++i) {
+				std::string serial = std::string(ctx.get_device(i)->get_serial());
+				std::cout << "Found connected device: " << serial << std::endl;
+				
+				if (_deviceMap.count(serial) == 0) {
+					// No recordings exist, create a folder
+				}
+				else {
+					_deviceMap[serial] = ctx.get_device(i);
+				}
+			}
+		}
+		else {
+			throw new std::invalid_argument("Invalid argument: Unable to open folder");
+		}*/
+	}
+
+	RealSenseWrapper::~RealSenseWrapper() {
+		// Stop all streams, close all cameras, destroy context
+	}
 }
 
-int main(void) {
-    int result = addTwo(1,2);
-    std::cout << result;
+using namespace std;
+
+int main(void) try {
+	rs::log_to_console(rs::log_severity::warn);
 	rs::context ctx;
-    return result;
+	//std::string dirName = "C:\\Users\\yimmy\\Documents\\work\\CAMP2016\\rswrapper_frames";
+	//rsw::RealSenseWrapper realsense(dirName);
+	std::cout << "Waiting...";
+	int i;
+	std::cin >> i;
+	return 0;
+}
+catch (const rs::error & e)
+{
+	std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const std::exception & e) {
+	std::cout << e.what() << std::endl;
+	return EXIT_FAILURE;
 }
--- src/rs_wrapper.h
+++ src/rs_wrapper.h
@@ -2,43 +2,50 @@
 #define RSWRAPPER_H
 
 #include <vector>
+#include <map>
 #include <string>
+#include <exception>
 
+#include <boost/filesystem.hpp>
 #include <rs.hpp>
 
-class RealSenseWrapper {
-public:
-	enum RSError {
+namespace fs = boost::filesystem;
+
+namespace rsw {
+	class RealSenseWrapper {
+	public:
+		enum RSError {
+		};
+
+		/// throws boost::filesystem::filesystem_error if unable to open directory
+		RealSenseWrapper(std::string directory);
+
+		~RealSenseWrapper();
+
+		/// Returns list of serial names each corresponding to a camera
+		std::vector<std::string> getDeviceList();
+
+		void printStatus();
+
+		/// Returns frame at specified timestamp of specified stream
+		RSError getFrame(const void * frame, std::string serial, rs::stream strm,
+			std::string streamName, int timestamp);
+
+		RSError startStream(std::string serial, rs::stream strm, std::string streamName, rs::preset preset);
+
+		RSError startStream(std::string serial, rs::stream strm, std::string streamName,
+			int width, int height, rs::format fmt, int framerate);
+
+		RSError stopStream(std::string serial, rs::stream strm);
+
+	private:
+		rs::device * getDevice(std::string serial);
+
+		int i;
+		//rs::context ctx;
+		fs::path dataPath;
+		std::map<std::string, rs::device *> _deviceMap;
 	};
-
-	RealSenseWrapper() {
-		// Initialize context, initialize list of cameras
-	}
-
-	~RealSenseWrapper() {
-		// Stop all streams, close all cameras, destroy context
-	}
-
-	/// Returns list of serial names each corresponding to a camera
-	std::vector<std::string> getDeviceList();
-
-	void printStatus();
-
-	/// Returns frame at specified timestamp of specified stream
-	RSError getFrame(const void * frame, std::string serial, rs::stream strm,
-					 std::string streamName, int timestamp);
-
-	RSError startStream(std::string serial, rs::stream strm, std::string streamName, rs::preset preset);
-
-	RSError startStream(std::string serial, rs::stream strm, std::string streamName,
-					 int width, int height, rs::format fmt, int framerate);
-
-	RSError stopStream(std::string serial, rs::stream strm);
-
-private:
-	rs::device * getDevice(std::string serial);
-
-	std::vector<rs::device *> _devices;
-};
+}
 
 #endif
