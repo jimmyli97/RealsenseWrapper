f16b3031cedcaf3e4c9ba4a17bac3e96928ae57a
Jimmy Li <jimmy.jiajian.li@gmail.com>
2016-06-13 17:54:57 -0400

    Working with plug in camera, thread start code written

--- src/rs_wrapper.cpp
+++ src/rs_wrapper.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
-#include <rs.hpp>
+#include <ios>
 
+#include "boost/filesystem/fstream.hpp"
 #include "rs_wrapper.h"
+#include "rs.hpp"
 
 namespace rsw {
 	RealSenseWrapper::RealSenseWrapper(std::string directory) :
@@ -18,8 +20,6 @@ namespace rsw {
 				_deviceMap[fn] = NULL;
 			}
 
-			fs::create_directory(dataPath / "1234");
-
 			// Find matching connected devices, add to map
 			for (int i = 0; i < ctx.get_device_count(); ++i) {
 				std::string serial = std::string(ctx.get_device(i)->get_serial());
@@ -29,7 +29,7 @@ namespace rsw {
 					// No recordings exist, create a folder
 					fs::create_directory(dataPath / serial);
 				}
-				_deviceMap[serial] = ctx.get_device(i);
+				_deviceMap[serial].first = ctx.get_device(i);
 			}
 		}
 		else {
@@ -38,7 +38,105 @@ namespace rsw {
 	}
 
 	RealSenseWrapper::~RealSenseWrapper() {
-		// Stop all streams, close all cameras, destroy context
+		// Join all threads, release all mutexes, stop all devices
+	}
+
+	std::vector<std::string>* RealSenseWrapper::getDeviceList() {
+		std::vector<std::string>* out = new std::vector<std::string>();
+		for (auto items : _deviceMap) {
+			out->push_back(items.first);
+		}
+		return out;
+	}
+
+	void RealSenseWrapper::printStatus() {
+		for (auto items : _deviceMap) {
+			rs::device* dev = items.second.first;
+			std::cout << items.first << ": " << std::endl;
+			if (dev == NULL) {
+				std::cout << "  Connected: No" << std::endl;
+			} else {
+				std::cout << "  Connected: Yes" << std::endl;
+				std::cout << "  Device: " << dev->get_name() << std::endl;
+				std::cout << "  Supported Streams: " << std::endl;
+
+				// Show which streams are supported by this device
+				for (int j = 0; j < rs_stream::RS_STREAM_COUNT; ++j) {
+					// Determine number of available streaming modes (zero means stream is unavailable) 
+					rs::stream strm = (rs::stream)j;
+					int mode_count = dev->get_stream_mode_count(strm);
+					if (mode_count == 0) continue;
+
+					// Show each available mode for this stream
+					std::cout << "    Stream " << strm << " - " << mode_count << " modes:\n";
+					for (int k = 0; k < mode_count; ++k) {
+						// Show width, height, format, and framerate, the settings required to enable the stream in this mode
+						int width, height, framerate;
+						rs::format format;
+						dev->get_stream_mode(strm, k, width, height, format, framerate);
+						std::cout << "      " << width << "\tx " << height << "\t@ " << framerate << "Hz\t" << format << std::endl;
+					}
+				}
+			}
+			std::cout << "  Available Playback:" << std::endl;
+			for (auto stream : fs::directory_iterator(dataPath / items.first)) {
+				std::cout << "    " << stream.path().stem() << std::endl;
+				for (auto strmName : fs::directory_iterator(stream.path())) {
+					std::cout << "      " << strmName.path().stem() << std::endl;
+				}
+			}
+		}
+	}
+
+	RealSenseWrapper::RSError RealSenseWrapper::getFrame(std::vector<char>** data,
+			std::string serial, rs::stream strm, std::string streamName, int timestamp) {
+		fs::path p = dataPath / serial / rs_stream_to_string((rs_stream) strm) / streamName;
+		// Check if timestamp exists/is currently being written to
+		auto writingInfo = _writeInfo.find(p);
+		if (writingInfo == _writeInfo.end() || (timestamp < writingInfo->second->second)) {
+			// Read path
+			fs::ifstream ifs(p / std::to_string(timestamp), std::ios::in | std::ios::binary);
+			std::streamsize size = ifs.tellg();
+			ifs.seekg(0, std::ios::beg);
+			auto readData = new std::vector<char>(size);
+			if (ifs.read(readData->data(), size)) {
+				*data = readData;
+				return NO_ERROR;
+			}
+		}
+		return UNABLE_TO_ACCESS;
+	}
+
+	void writeImg(fs::path p, std::string serial, rs::stream strm) {
+		// acquire mutex on device
+		// poll for images
+		// write to path if images available with corresponding timestamp
+		// update timestamp in map
+		// release mutex
+	}
+
+	RealSenseWrapper::RSError RealSenseWrapper::startStream(std::string serial,
+			rs::stream strm, std::string streamName, int width, int height,
+			rs::format fmt, int framerate) {
+		fs::path p(dataPath / serial / rs_stream_to_string((rs_stream)strm) / streamName);
+		// Check if we're writing to this stream already or if the name already exists
+		if (_writeInfo.find(p) == _writeInfo.end() && !fs::exists(p)) {
+			_deviceMap[serial].first->enable_stream(strm, width, height, fmt, framerate);
+			_deviceMap[serial].first->start;
+
+			// Create folders
+			fs::create_directories(p);
+
+			// Start thread 
+			auto writeThread = new std::thread(writeImg, p, serial, strm);
+			return NO_ERROR;
+		} else {
+			return UNABLE_TO_ACCESS;
+		}
+	}
+
+	RealSenseWrapper::RSError RealSenseWrapper::stopStream(std::string serial, rs::stream strm) {
+		return NO_ERROR;
 	}
 }
 
@@ -49,14 +147,16 @@ int main(void) try {
 	rsw::RealSenseWrapper realsense(dirName);
 
 	// TEST: get list of devices
-	std::vector<std::string> deviceList = realsense.getDeviceList();
-	for (auto str : deviceList) {
+	std::vector<std::string>* deviceList = realsense.getDeviceList();
+	for (auto str : *deviceList) {
 		std::cout << "Device Serial: " << str << std::endl;
 	}
+	delete deviceList;
 
 	// TEST: print status of devices (attached/not attached, available playback, if stream is streaming)
 	realsense.printStatus();
 
+	/*
 	// TEST: frame does not exist error
 	void * data;
 	int ret = realsense.getFrame(data, "serial", rs::stream::color, "serial_color", 102);
@@ -74,6 +174,7 @@ int main(void) try {
 	ret = realsense.getFrame(data, "serial", rs::stream::depth, "serial_depth", 204);
 
 	// TODO: test stream name conflicts
+	*/
 	std::cout << "Waiting...";
 	std::string i;
 	std::cin >> i;
--- src/rs_wrapper.h
+++ src/rs_wrapper.h
@@ -2,9 +2,13 @@
 #define RSWRAPPER_H
 
 #include <vector>
+#include <tuple>
 #include <map>
 #include <string>
 #include <exception>
+#include <thread>
+#include <mutex>
+#include <atomic>
 
 #include <boost/filesystem.hpp>
 #include <rs.hpp>
@@ -15,23 +19,24 @@ namespace rsw {
 	class RealSenseWrapper {
 	public:
 		enum RSError {
+			NO_ERROR = 0,
+			UNABLE_TO_ACCESS
 		};
 
 		/// throws boost::filesystem::filesystem_error if unable to open directory
+		/// throws std::invalid_argument if directory does not exist
 		RealSenseWrapper(std::string directory);
 
 		~RealSenseWrapper();
 
 		/// Returns list of serial names each corresponding to a camera
-		std::vector<std::string> getDeviceList();
+		std::vector<std::string>* getDeviceList();
 
 		void printStatus();
 
 		/// Returns frame at specified timestamp of specified stream
-		RSError getFrame(void * data, std::string serial, rs::stream strm,
+		RSError getFrame(std::vector<char>** data, std::string serial, rs::stream strm,
 			std::string streamName, int timestamp);
-
-		RSError startStream(std::string serial, rs::stream strm, std::string streamName, rs::preset preset);
 
 		RSError startStream(std::string serial, rs::stream strm, std::string streamName,
 			int width, int height, rs::format fmt, int framerate);
@@ -39,12 +44,13 @@ namespace rsw {
 		RSError stopStream(std::string serial, rs::stream strm);
 
 	private:
-		rs::device * getDevice(std::string serial);
-
 		int i;
 		rs::context ctx;
 		fs::path dataPath;
-		std::map<std::string, rs::device *> _deviceMap;
+		// map of serial strings to devices and mutexes. device is NULL if it's disconnected
+		std::map<std::string, pair<rs::device*, std::mutex*>> _deviceMap;
+		// map of all paths currently being written to and their corresponding timestamp and thread
+		std::map <fs::path, pair<std::thread*, std::atomic_int>> _writeInfo;
 	};
 }
 
